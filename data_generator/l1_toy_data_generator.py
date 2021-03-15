cee0c934ba79a4e47b40eab369ac6320fb2b03c4
wdiepeveen <willem_diep@hotmail.com>
2021-03-15 17:33:56 +0100

    Prox learning

--- data_generator/l1_toy_data_generator.py
+++ data_generator/l1_toy_data_generator.py
@@ -11,6 +11,9 @@ data_dim = 10
 size_training_data = 500
 size_test_data = 100
 
+size_sup_training_data = 1000
+size_sup_test_data = 20
+
 # Set seed
 np.random.seed(seed=0)
 
@@ -18,8 +21,24 @@ results_dir = os.path.join("..", "data", "l1_toy_data")
 prior_dir = "groundtruth"
 # fidelity_dir
 
+labeled_data_dir = "labeled"
+
 # Generate training data
 training_dir = os.path.join(results_dir, "train")
+
+def mye_l1(lambd,x):
+    z = np.zeros(x.shape)
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            if np.abs(x[i,j]) < lambd:
+                z[i,j] = 1/(2*lambd) * x[i,j]**2
+            else:
+                z[i,j] = np.abs(x[i,j]) - lambd/2
+
+    mye = np.sum(z)
+
+    return mye
+
 
 for i in range(size_training_data):
     # Generate prior data
@@ -28,6 +47,14 @@ for i in range(size_training_data):
     np.save(os.path.join(training_dir, prior_dir, filename + ".npy"), sample)
 
     # Generate l2 fidelity data
+
+for i in range(size_sup_training_data):
+    # Generate labels
+    labeled_sample = np.random.laplace(loc=0, scale=1, size=(data_dim, data_dim))
+    label = np.sum(mye_l1(1, labeled_sample))
+    filename = 'sample_{}'.format(str(i + 1).zfill(4))
+    np.save(os.path.join(training_dir, labeled_data_dir, "data", filename + ".npy"), labeled_sample)
+    np.save(os.path.join(training_dir, labeled_data_dir, "labels", filename + ".npy"), label)
 
 
 # Generate test data
@@ -40,3 +67,12 @@ for i in range(size_test_data):
     np.save(os.path.join(test_dir, prior_dir, filename + ".npy"), sample)
 
     # Generate l2 fidelity data
+
+for i in range(size_sup_test_data):
+    # Generate labels
+    labeled_sample = np.random.laplace(loc=0, scale=1, size=(data_dim, data_dim))
+    label = mye_l1(1, labeled_sample)
+    filename = 'sample_{}'.format(str(i + 1).zfill(4))
+    np.save(os.path.join(test_dir, labeled_data_dir, "data", filename + ".npy"), labeled_sample)
+    np.save(os.path.join(test_dir, labeled_data_dir, "labels", filename + ".npy"), label)
+
