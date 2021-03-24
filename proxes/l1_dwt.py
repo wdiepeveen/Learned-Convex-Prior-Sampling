8d8fd0772f31f319f64a8bf4979caf517a0d57e5
wdiepeveen <willem_diep@hotmail.com>
2021-03-24 17:37:16 +0100

    wavelet myula

--- proxes/l1_dwt.py
+++ proxes/l1_dwt.py
@@ -1,5 +1,16 @@
-def prox_l1_dwt(lambd, x, var=None):
+import pywt
 
-    
+from proxes.l1_norm import prox_l1_norm
 
+
+def prox_l1_dwt(lambd, x, level=2):
+    v = pywt.wavedec2(x, 'db4', mode='zero', level=level)  # wavelet transform x
+    for l in range(level+1):
+        if l==0:
+            v[0] = prox_l1_norm(lambd, v[0])
+        else:
+            v[l] = [prox_l1_norm(lambd, coeff) for coeff in v[l]]  # soft shrinkage in wavelet domain
+    prox = pywt.waverec2(v, 'db4', mode='zero')  # transform wavelet prox back to image domain
+    # periodization
     return prox
+
--- proxes/l1_norm.py
+++ proxes/l1_norm.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-def prox_l1_norm(lambd, x, var=None):
+def prox_l1_norm(lambd, x):
     """
-    Computes prox_{\lambda f}(x) for  f(x) = \frac{1}{2}\|x\|_1, which is given by u_i = sign(x_i) max(0, |x_i| - \lambda)
+    Computes prox_{\lambda f}(x) for  f(x) = \|x\|_1, which is given by u_i = sign(x_i) max(0, |x_i| - \lambda)
     :param x: current iterate
     :return: [ sign(x_i) * max(0, abs(x_i) - lambd) for x_i in x ]
     """
-    # TODO variance must be matrix
-    # TODO if var is None assume that we have unit variance
-    return np.sign(x) * np.fmax(np.zeros(len(x)), np.fabs(x) - lambd)
+    return np.sign(x) * np.fmax(np.zeros(x.shape), np.fabs(x) - lambd)
